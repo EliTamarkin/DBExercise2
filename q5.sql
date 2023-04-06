120a2869ef9147ad68630ddc5a157966b36d2605
EliTamarkin <elitamarkin98@gmail.com>
2023-04-06 15:44:04 +0300

    q5

--- q5.sql
+++ q5.sql
@@ -8,4 +8,5 @@ FROM (SELECT uid, name, number as number1
         FROM members NATURAL JOIN memberInKnesset) AS R NATURAL JOIN (SELECT uid, name, number as number2
                                                                     FROM members NATURAL JOIN memberInKnesset) AS R
 WHERE number1 != number2
-)
+) AS P
+ORDER BY name ASC
