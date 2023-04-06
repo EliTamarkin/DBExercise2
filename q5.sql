150ef21e8e393dd49cf5895be3c50b9af8c5d932
EliTamarkin <elitamarkin98@gmail.com>
2023-04-06 15:46:03 +0300

    q5

--- q5.sql
+++ q5.sql
@@ -5,8 +5,8 @@ FROM members
 EXCEPT
 SELECT DISTINCT uid, name
 FROM (SELECT uid, name, number as number1
-        FROM members NATURAL JOIN memberInKnesset) AS R NATURAL JOIN (SELECT uid, name, number as number2
-                                                                    FROM members NATURAL JOIN memberInKnesset) AS R
+        FROM members NATURAL JOIN memberInKnesset) AS R1 NATURAL JOIN (SELECT uid, name, number as number2
+                                                                    FROM members NATURAL JOIN memberInKnesset) AS R2
 WHERE number1 != number2
 ) AS P
 ORDER BY name ASC
